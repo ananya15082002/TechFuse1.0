37940a7cebb4af3bd518ec702c7a389a9a51ca86
Ananya Singh <117035260+ananya15082002@users.noreply.github.com>
2023-04-19 23:53:22 +0530

    Add files via upload

---
+++ symptoms_list.js
@@ -0,0 +1,15 @@
+// JS CODE FOR SEARCH BAR 
+function search_symptom() { 
+	let input = document.getElementById('searchbar').value 
+	input=input.toLowerCase(); 
+	let x = document.getElementsByClassName('symptoms'); 
+	
+	for (i = 0; i < x.length; i++) { 
+		if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+			x[i].style.display="none"; 
+		} 
+		else { 
+			x[i].style.display="list-item";				 
+		} 
+	} 
+} 
